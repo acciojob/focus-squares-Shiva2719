f978e1e408577b2b79e2598649f34e71b415c929
Shiva2719 <162423616+Shiva2719@users.noreply.github.com>
2025-09-30 22:09:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 //your JS code here. If required.
+const squares = document.querySelectorAll('.square');
+
+squares.forEach(focusedSquare => {
+    focusedSquare.addEventListener('mouseenter', () => {
+        squares.forEach(square => {
+            if (square === focusedSquare) {
+                square.style.backgroundColor = '#E6E6FA'; 
+            } else {
+                square.style.backgroundColor = '#6F4E37'; 
+            }
+        });
+    });
+
+    focusedSquare.addEventListener('mouseleave', () => {
+        squares.forEach(square => {
+            square.style.backgroundColor = '#E6E6FA'; // lavender
+        });
+    });
+});
